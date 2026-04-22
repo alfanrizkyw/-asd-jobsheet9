3b12dd98b318d763f8634915301f2949dc85f620
Alfan Rizky <alffanr54321@gmail.com>
2026-04-22 10:25:52 +0700

    Delete asd jobsheat9 directory

--- asd jobsheat9/Mahasiswa23.py
+++
@@ -1,9 +0,0 @@
-class Mahasiswa23:
-    def __init__(self, nama, nim, kelas):
-        self.nama = nama
-        self.nim = nim
-        self.kelas = kelas
-        self.nilai = -1
-
-    def tugasDinilai(self, nilai):
-        self.nilai = nilai
